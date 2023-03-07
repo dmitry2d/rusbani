58f4aeda183753d196d8ba18f2c5945b0095eb92
Dmitry <volcharo@gmail.com>
2023-03-07 20:21:48 +0300

    code

--- templates/section-header.php
+++ templates/section-header.php
@@ -1,4 +1,3 @@
-
 <?php
 /*
 * Section Header
@@ -11,6 +10,14 @@
         <meta charset="utf-8">
         <meta name=”format-detection” content=”telephone=no”>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+
         <title><?php wp_title(); ?></title>
         <?php wp_head(); ?>
 
